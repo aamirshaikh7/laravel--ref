4859c3eb0b6306d19272a86202998f615c04e5d4
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-01-19 23:46:20 +0530

    Database - using Eloquent Model to query the database

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,15 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
     public function show ($slug) {
-        $post = DB::table('posts')->where('slug', $slug)->first();
-    
-        if (! $post) {
-            abort(404);
-        }
+        $post = Post::where('slug', $slug)->firstOrFail();
 
         return view('post', ['post' => $post]);
     }
